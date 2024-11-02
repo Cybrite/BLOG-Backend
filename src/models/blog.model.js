91d313601339be2c30d2f426547a5b3eaf0a6e1b
Harsh <harshtanishq2002@gmail.com>
2024-11-02 19:04:16 +0530

    feat: added blog schema

--- src/models/blog.model.js
+++ src/models/blog.model.js
@@ -1,5 +1,44 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const blogSchema = new mongoose.Schema({}, { timestamps: true });
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    subDescription: {
+      type: String,
+      lowercase: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    bgImage: {
+      type: String,
+      required: true,
+    },
+    isPublsihed: {
+      type: Boolean,
+      default: false,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+blogSchema.plugin(mongooseAggregatePaginate);
 
 export default Blog = mongoose.model("Blog", blogSchema);
